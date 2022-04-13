592d3ce1f6b6961d4a876b041f3b7dcd786d4cef
04irsyaD <98296563+04irsyaD@users.noreply.github.com>
2022-04-13 11:40:15 +0700

    first commit

---
+++ src/latihan2/Latihan2.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Irsyad
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int ang1;
+        
+        System.out.println("Cth input A :");
+        ang1 = input.nextInt();
+       
+        
+        if (ang1%2==0){
+            System.out.println("Genab"+ang1);
+            
+            
+        }else {
+            System.out.println("Ganjil"+ang1);
+        }
+        
+        
+       
+               
+        
+                }
+    
+        
+        
+    }
+    
+
